508fa6c95d2dff8ed858a2a03b142939a5dc4d3b
mangix <maqh1988@gmail.com>
2015-04-26 20:45:51 +0800

    db

--- service/common/connection.js
+++ service/common/connection.js
@@ -1,13 +1,5 @@
 var mysql = require('mysql');
-var pool = mysql.createPool({
-    host:"127.0.0.1",
-    user:"root",
-    password:"123456",
-    database: "playball",
-    connectionLimit: 15,
-    acquireTimeout:20000,
-    debug: false
-});
+var pool = mysql.createPool(require("./db"));
 
 var query = function () {
     /*parse params as same as connection.query*/
---
+++ service/common/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host:"127.0.0.1",
+    user: "root",
+    password: "123456",
+    database: "playball",
+    connectionLimit: 15,
+    acquireTimeout: 20000,
+    debug: false
+};
